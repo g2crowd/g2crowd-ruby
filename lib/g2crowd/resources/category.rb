7e1be40ccc081f92eb111bd1fa9c142010b3dddd
Lachlan Priest <lachlanpriest@gmail.com>
2018-02-06 19:01:04 +0800

    Add extra categories

--- lib/g2crowd/resources/category.rb
+++ lib/g2crowd/resources/category.rb
@@ -1,5 +1,9 @@
 module G2crowd
   class Category < Base
     has_many :products
+    has_many :children, class_name: 'Category'
+    has_many :ancestors, class_name: 'Category'
+    has_many :descendants, class_name: 'Category'
+    has_one :parent, class_name: 'Category'
   end
 end
--- lib/g2crowd/resources/product.rb
+++ lib/g2crowd/resources/product.rb
@@ -2,5 +2,6 @@ module G2crowd
   class Product < Base
     has_many :survey_responses
     has_many :categories
+    has_one :main_category, class_name: 'Category'
   end
 end
